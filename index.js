5aa1620b4ac20379d529341ea6c0a4e88ee99120
Alex Bannow <rbannal@gmail.com>
2019-10-05 18:53:25 -0400

    added jquery to handle buttons

---
+++ index.js
@@ -0,0 +1,29 @@
+const menuExpand = function() {
+  $('#nav-button').click(e => {
+    $('#nav-links').toggleClass('hidden');
+  });
+};
+
+
+const projectExpand = function() {
+  $('#project-one').click(e => {
+    $('#content-one').toggleClass('hidden');
+  });
+  $('#project-two').click(e => {
+    $('#content-two').toggleClass('hidden');
+  });
+  $('#project-three').click(e => {
+    $('#content-three').toggleClass('hidden');
+  });
+  $('#project-four').click(e => {
+    $('#content-four').toggleClass('hidden');
+  });
+};
+
+
+const main = function() {
+  menuExpand();
+  projectExpand();
+};
+
+$(main);
